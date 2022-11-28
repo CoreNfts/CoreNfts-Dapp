f88635f64f2d31a54970e343b745ab0604a55843
CoreNfts <119289384+CoreNfts@users.noreply.github.com>
2022-11-28 14:46:36 +0100

    Update dataReducer.js

--- src/redux/data/dataReducer.js
+++ src/redux/data/dataReducer.js
@@ -1,6 +1,6 @@
 const initialState = {
   loading: false,
-  totalSupply: 0,
+  Supply: 0,
   cost: 0,
   error: false,
   errorMsg: "",
@@ -19,7 +19,7 @@ const dataReducer = (state = initialState, action) => {
       return {
         ...state,
         loading: false,
-        totalSupply: action.payload.totalSupply,
+        Supply: action.payload.Supply,
         // cost: action.payload.cost,
         error: false,
         errorMsg: "",
