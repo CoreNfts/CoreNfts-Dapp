0a521048ef42a622e901a06a96c0d83cd4669ad9
CoreNfts <119289384+CoreNfts@users.noreply.github.com>
2023-02-16 16:00:41 +0100

    Update stakedataReducer.js

--- src/redux/data/stakedataReducer.js
+++ src/redux/data/stakedataReducer.js
@@ -6,7 +6,7 @@ const initialState = {
   errorMsg: "",
 };
 
-const dataReducer = (state = initialState, action) => {
+const stakedataReducer = (state = initialState, action) => {
   switch (action.type) {
     case "CHECK_DATA_REQUEST":
       return {
