387c8342f503b38b7e900f3cd46d246ccf30eb66
CoreNfts <119289384+CoreNfts@users.noreply.github.com>
2023-02-20 11:27:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -386,7 +386,6 @@ function App() {
                         e.preventDefault();
                         dispatch(connect2());
                         getData();
-                      }
                       .then() => {
                         approveStake();
                       }}
