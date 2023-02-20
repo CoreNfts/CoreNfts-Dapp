bac9309eba223d5f5c01f4e4f790be9dc93da613
CoreNfts <119289384+CoreNfts@users.noreply.github.com>
2023-02-20 05:32:59 +0100

    Update dataActions.js

--- src/redux/data/dataActions.js
+++ src/redux/data/dataActions.js
@@ -25,9 +25,9 @@ export const fetchData = () => {
   return async (dispatch) => {
     dispatch(fetchDataRequest());
     try {
-      let Stakers = await store
+      let Supply = await store
         .getState()
-        .blockchain.smartContract.methods.Stakers()
+        .blockchain.smartContract.methods.Supply()
         .call();
       // let cost = await store
       //   .getState()
@@ -36,7 +36,7 @@ export const fetchData = () => {
 
       dispatch(
         fetchDataSuccess({
-          Stakers,
+          Supply,
           // cost,
         })
       );
