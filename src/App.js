1acd7e193aa20b02c95d2f9f04c3c8dff1bf27df
CoreNfts <119289384+CoreNfts@users.noreply.github.com>
2023-02-21 12:57:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ function App() {
   const blockchain = useSelector((state) => state.blockchain);
   const data = useSelector((state) => state.data);
   const [claimingNft, setClaimingNft] = useState(false);
-  const [feedback, setFeedback] = useState(`key in your tokenId.`);
+  const [feedback, setFeedback] = useState(`Click buy to proceed.`);
   const [tokenId, setTokenId] = useState(120);
   const [CONFIG, SET_CONFIG] = useState({
     CONTRACT_ADDRESS: "",
