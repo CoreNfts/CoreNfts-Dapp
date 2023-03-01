753289778216f654d8b671a3bce58c9a110877aa
CoreNfts <119289384+CoreNfts@users.noreply.github.com>
2023-03-01 05:52:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -406,7 +406,7 @@ function App() {
                     <StyledButton
                       onClick={(e) => {
                         e.preventDefault();
-                        dispatch(connect());
+                        dispatch(connect2());
                         getData();
                       }}
                     >
